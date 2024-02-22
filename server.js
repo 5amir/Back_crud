cac175abda8d04c8095f9cbca559440dfd18439e
Amir <awillosse@gmail.com>
2024-02-22 19:51:35 +0100

    ajout de fonctionnalité upload image non avancé

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require("express");
-const mysql = require("mysql");
-const cors = require("cors");
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
+import multer from "multer";
+import path from "path";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -44,10 +47,35 @@ app.put("/update/:id", (req, res) => {
 app.delete("/delete/:id", (req, res) => {
   const sql = "DELETE FROM users WHERE id = ?";
   const id = req.params.id;
-
   db.query(sql, [id], (err, data) => {
     if (err) return res.json(err);
     return res.json("deleted");
+  });
+});
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  // console.log(req.file);
+  const image = req.file.filename;
+  const sql = "UPDATE users SET image = ?";
+  db.query(sql, [image], (err, resultat) => {
+    if (err) return res.json({ Message: "Error" });
+    return res.json({ Status: "Success" });
   });
 });
 
